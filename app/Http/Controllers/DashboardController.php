579493c6741690854a943c902ce33fe608f158d2
AntonDePaz <pazjeck14@gmail.com>
2022-12-05 09:37:41 +0800

    2nd Changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
+        if(empty(session('id'))){
+            return redirect('login');
+        }
         $faculty = Faculty::all();
         return view('admin.index', compact('faculty'));
     }
