24a536a326efbdae3a5903079b53a25cd1350d98
thonexdp <pazjeck14@gmail.com>
2023-01-11 01:53:25 -0800

    chafew

--- database/dbnew.sql
+++ database/dbnew.sql
@@ -12,9 +12,9 @@ MySQL - 10.4.24-MariaDB : Database - faculty_monitoring
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`faculty_monitoring` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`slsufms` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `faculty_monitoring`;
+USE `slsufms`;
 
 /*Table structure for table `account` */
 
